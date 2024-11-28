771354b8a9db8e8f74d621d9ac218352c43a4b63
Ilya Golubkov <golubkov@mcad.ru>
2024-11-28 08:25:44 +0600

    Add: new image menu

--- pixl/pxcanvas/pxcanvas.go
+++ pixl/pxcanvas/pxcanvas.go
@@ -112,3 +112,22 @@ func (pxCanvas *PxCanvas) MouseToCanvasXY(ev *desktop.MouseEvent) (*int, *int) {
 	y := int((ev.PointEvent.Position.Y - yOffset) / pxSize)
 	return &x, &y
 }
+
+func (pxCanvas *PxCanvas) LoadImage(img image.Image) {
+	dimensions := img.Bounds()
+
+	pxCanvas.PxCanvasConfig.PxCols = dimensions.Dx()
+	pxCanvas.PxCanvasConfig.PxRows = dimensions.Dy()
+
+	pxCanvas.PixelData = img
+	pxCanvas.reloadImage = true
+	pxCanvas.Refresh()
+}
+
+func (pxCanvas *PxCanvas) NewDrawing(cols, rows int) {
+	pxCanvas.appState.SetFilePath("")
+	pxCanvas.PxCols = cols
+	pxCanvas.PxRows = rows
+	pixelData := NewBlancImage(cols, rows, color.NRGBA{128, 128, 128, 255})
+	pxCanvas.LoadImage(pixelData)
+}
--- pixl/ui/layout.go
+++ pixl/ui/layout.go
@@ -3,6 +3,7 @@ package ui
 import "fyne.io/fyne/v2/container"
 
 func Setup(app *AppInit) {
+	SetupMenus(app)
 	swiatchesContainer := BuildSwatches(app)
 	colorPicker := SetupColorPicker(app)
 	appLayout := container.NewBorder(nil, swiatchesContainer, nil, colorPicker, app.PixlCanvas)
